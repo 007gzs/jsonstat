52bf0b966623ab3703d1fa4aac1d275125d8806c
007gzs <007gzs@gmail.com>
2025-04-03 15:07:23 +0800

    fmt

--- examples/json_file_stat_mt.rs
+++ examples/json_file_stat_mt.rs
@@ -7,43 +7,46 @@ use std::sync::mpsc::channel;
 use jsonstat::JsonStat;
 use threadpool::ThreadPool;
 
-struct PathWalk{
+struct PathWalk {
     ext: String,
     paths: Vec<String>,
-    pool: ThreadPool
+    pool: ThreadPool,
 }
 impl PathWalk {
     fn new(args: Vec<String>) -> PathWalk {
-        PathWalk { 
-            ext: args[1].to_lowercase(), 
-            paths:args.iter().skip(2).map(|s| s.to_string()).collect(),
-            pool: ThreadPool::new(num_cpus::get())
-         }
+        PathWalk {
+            ext: args[1].to_lowercase(),
+            paths: args.iter().skip(2).map(|s| s.to_string()).collect(),
+            pool: ThreadPool::new(num_cpus::get()),
+        }
     }
-    fn run(&mut self) -> JsonStat{
+    fn run(&mut self) -> JsonStat {
         let (tx, rx) = channel();
 
-        while !self.paths.is_empty(){
+        while !self.paths.is_empty() {
             let path = self.paths.pop().unwrap();
             let p = Path::new(&path);
-            if !p.exists(){
+            if !p.exists() {
                 continue;
             }
-            if p.is_dir(){
+            if p.is_dir() {
                 for entry in p.read_dir().expect("read dir error") {
                     let entry = entry.expect("read dir entry error");
-                    let path = entry.path().to_str().expect("path to str error").to_string();
+                    let path = entry
+                        .path()
+                        .to_str()
+                        .expect("path to str error")
+                        .to_string();
                     println!("new path {}", path);
                     self.paths.push(path);
-                    
                 }
                 continue;
             }
-            if !p.is_file(){
+            if !p.is_file() {
                 continue;
             }
-            if !path.to_lowercase().ends_with(&self.ext){
-                continue
+            if !path.to_lowercase().ends_with(&self.ext) {
+                continue;
             }
             let _tx = tx.clone();
             self.pool.execute(move || {
@@ -74,5 +77,4 @@ fn main() {
     let stat = path_walk.run();
 
     println!("{}", stat);
-
 }
